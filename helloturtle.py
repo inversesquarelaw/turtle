8c560c9b0a6ec9b9326e0e15cc62dc45879b9081
The Law <email@example.fake>
2014-05-29 17:51:14 -0700

    Implemented drawSpiral

--- helloturtle.py
+++ helloturtle.py
@@ -40,10 +40,27 @@ def drawCircle(points, color, myTurtle):
 
 def drawLine(points, color, myTurtle):
     myTurtle.pencolor(color)
+    myTurtle.pensize(points[2][0])
     myTurtle.up()
     myTurtle.goto(points[0][0],points[0][1])
     myTurtle.down()
     myTurtle.goto(points[1][0],points[1][1])
+    myTurtle.pencolor('black')
+    myTurtle.pensize(1)
+
+def spiralHelper(lineLen, myTurtle):
+    if lineLen > 0:
+        myTurtle.forward(lineLen)
+        myTurtle.right(90)
+        spiralHelper(lineLen-5, myTurtle)
+
+def drawSpiral(points, color, myTurtle):
+    myTurtle.pencolor(color)
+    myTurtle.up()
+    myTurtle.goto(points[0][0],points[0][1])
+    myTurtle.down()
+    spiralHelper(points[1][0], myTurtle)
+    myTurtle.pencolor('black')
 
 def main():
     myTurtle = turtle.Turtle()
@@ -61,9 +78,16 @@ def main():
     myCircle = [[100, 100, 150]]
     drawCircle(myCircle, 'red', myTurtle)
     
-    #           x1, y1     x2, y2
-    myLine = [[-400,400],[-300,300]]
+    #           x1, y1     x2, y2  ,thickness
+    myLine = [[-400,400],[-300,300],[5]]
     drawLine(myLine, 'blue', myTurtle)
+
+    myTriangle = [[-400,-300],[-400,-250],[-350,-200]]
+    drawTriangle(myTriangle, 'black', myTurtle)
+
+    #            start   , length
+    mySpiral = [[300,-300],[100]]
+    drawSpiral(mySpiral, 'black', myTurtle)
 
     myWin.exitonclick()
 
